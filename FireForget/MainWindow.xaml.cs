7ad220975cfc7a8248e3a52965033b8e7ad7f8c5
Simone Palugini <simonepalugini02@gmail.com>
2020-04-27 14:56:12 +0200

    Add project files.

---
+++ FireForget/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibraryTask;
+
+namespace FireForget
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        CancellationTokenSource cts;
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            Worker wrk = new Worker(10, 1000, cts);
+            wrk.Start();
+            MessageBox.Show("Mi dimentico del thread secondario e non lo attendo per visualizzare questo messaggio");
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (cts != null)
+                cts.Cancel();
+        }
+    }
+}
---
+++ LibraryTask/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryTask
+{
+    public class Worker
+    {
+        CancellationTokenSource _cts;
+        int _max;
+        int _delay;
+
+        //costruttore
+        public Worker(int max, int delay, CancellationTokenSource cts)
+        {
+            this._max = max;
+            this._delay = delay;
+            this._cts = cts;
+        }
+
+        public void Start()
+        {
+            Task.Factory.StartNew(DoWork);
+        }
+
+        private void DoWork()
+        {
+            for (int i = 0; i < _max; i++)
+            {
+                Thread.Sleep(_delay);
+                if (_cts.IsCancellationRequested)
+                    break;
+            }
+        }
+    }
+}
